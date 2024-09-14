8b4b3279ef61a63789a8c9dee5ff1b642e9a9621
karimamnr <karimamnr130@gmail.com>
2024-09-14 17:12:01 +0200

    weather

--- index.js
+++ index.js
@@ -1,45 +1,67 @@
-const choices = [ "rock" , "paper" , "scissors" ];
-const playerDisplay = document.getElementById('playerDisplay');
-const computerDisplay = document.getElementById('computerDisplay');
-const resultDisplay = document.getElementById('resultDisplay');
-const PlayScoreDisplay = document.getElementById('PlayScoreDisplay');
-const ComputerScoreDisplay = document.getElementById('ComputerScoreDisplay');
-let playScore = 0 ;
-let computerScore = 0;
-
-
-function playGame( playerChoice){
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
-    if(playerChoice === computerChoice){
-        result = " IT'S A TIE !";
-    }else{
-        switch(playerChoice){
-            case "rock" :
-                result =(computerChoice === "scissors") ? " YOU WIN !" : " YOU LOSE !";
-                break;
-            case "paper" :
-                result = (computerChoice === "rock") ? " YOU WIN !" : " YOU LOSE !";
-                break;
-            case "scissors" :
-                result = (computerChoice === "paper") ? " YOU WIN !" : " YOU LOSE !";
-                 break;
+const weatherForm = document.querySelector(".meatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "";
+weatherForm.addEventListener("submit" , async event => {
+    event.preventDefault();
+    const city = cityInput.value ;
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
         }
-    }
-    playerDisplay.textContent = ` PLAYER : ${playerChoice}`;
-    computerDisplay.textContent = ` COMPUTER : ${computerChoice}`;
-    resultDisplay.textContent = result ;
-    switch(result){
-        case " YOU WIN !":
-            resultDisplay.classList.add("greenText");
-            playScore ++ ;
-            PlayScoreDisplay.textContent = `  ${playScore}`;
-            break;
-        case " YOU LOSE !":
-            resultDisplay.classList.add("redText");
-            computerScore ++;
-            ComputerScoreDisplay.textContent =`  ${computerScore}` ;
-            break;
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
 
+    }else{
+        displayError("Please enter a city !");
     }
+})
+async function getWeatherData(city){
+   const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid =${apiKey}`;
+   const response = await fetch(apiURL);
+   if(!response.ok){
+    throw new Error("couldn't fetch weather data ");
+   }
+   return await response.json();
+}
+function displayWeatherInfo(data){
+   const {name: city , main: {temp, humidity}, weather: [{description, id}]} = data ;
+   card.textContent="";
+   card.style.display = "flex";
+
+   const cityDisplay = document.createElement("h1");
+   const tempDisplay = document.createElement("p");
+   const humidityDisplay  = document.createElement("p");
+   const descDisplay = document.createElement("p");
+   const weatherEmoji = document.createElement("p");
+
+   cityDisplay.textContent = city ;
+   tempDisplay.textContent = `${temp}°K `;
+
+   cityDisplay.classList.add("cityDisplay");
+   tempDisplay.classList.add("tempDisplay");
+
+   card.appendChild(cityDisplay);
+   card.appendChild(tempDisplay);
+
+
+}
+
+function getWeatherEmoji(weatherID){
+
+}
+
+
+function displayError(message){
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent="";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
 
 }
