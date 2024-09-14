9fe4728a383d6deb659c2ee36f24df8ea37dca0c
karimamnr <karimamnr130@gmail.com>
2024-09-14 17:02:44 +0200

    Rock - Paper - Scissors

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-const display = document.getElementById(`display`);
+const choices = [ "rock" , "paper" , "scissors" ];
+const playerDisplay = document.getElementById('playerDisplay');
+const computerDisplay = document.getElementById('computerDisplay');
+const resultDisplay = document.getElementById('resultDisplay');
+const PlayScoreDisplay = document.getElementById('PlayScoreDisplay');
+const ComputerScoreDisplay = document.getElementById('ComputerScoreDisplay');
+let playScore = 0 ;
+let computerScore = 0;
 
-function appendToDisplay(input){
-   display.value += input ;
-}
-function calculate(){
-   try{
-       display.value = eval(display.value);
-   }
-   catch(error){
-       display.value = "Error";
-   }
 
-}
-function clearDisplay(){
-   display.value = "";
+function playGame( playerChoice){
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    if(playerChoice === computerChoice){
+        result = " IT'S A TIE !";
+    }else{
+        switch(playerChoice){
+            case "rock" :
+                result =(computerChoice === "scissors") ? " YOU WIN !" : " YOU LOSE !";
+                break;
+            case "paper" :
+                result = (computerChoice === "rock") ? " YOU WIN !" : " YOU LOSE !";
+                break;
+            case "scissors" :
+                result = (computerChoice === "paper") ? " YOU WIN !" : " YOU LOSE !";
+                 break;
+        }
+    }
+    playerDisplay.textContent = ` PLAYER : ${playerChoice}`;
+    computerDisplay.textContent = ` COMPUTER : ${computerChoice}`;
+    resultDisplay.textContent = result ;
+    switch(result){
+        case " YOU WIN !":
+            resultDisplay.classList.add("greenText");
+            playScore ++ ;
+            PlayScoreDisplay.textContent = `  ${playScore}`;
+            break;
+        case " YOU LOSE !":
+            resultDisplay.classList.add("redText");
+            computerScore ++;
+            ComputerScoreDisplay.textContent =`  ${computerScore}` ;
+            break;
+
+    }
 
 }
